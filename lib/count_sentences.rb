cfd73ac06e091ffbf5f13b75e30b4f735eac8ed0
Muhammad Rendusara <hadyanrendusara@yahoo.com>
2021-12-17 19:34:07 -0600

    commit tree 52eb14eab74c5e595e231276d33337d57f60c73d

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,21 @@ require 'pry'
 
 class String
 
+  attr_accessor :sentence
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split('.').count + self.split('!').count + self.split('?').count
   end
 end
